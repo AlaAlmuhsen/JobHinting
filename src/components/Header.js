469f64ed07ec0ad1b7ff024f2e3a1a741c812dcd
dania-atieh <dania.a.atieh@gmail.com>
2023-06-19 15:18:45 +0300

    added a profile photo to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = ({active}) => {
-    return (
-        <header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container">
-                <Link className="navbar-brand" to='/'>JobHuntly</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link to='/findjob' className={"nav-link ".concat(active === 'find'?'active':'')} aria-current="page" href="#">FindJobs</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/browsecompanies' className={"nav-link ".concat(active === 'browse'?'active':'')} >Browse Companies</Link>
-                    </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <Link className="btn" type="submit">Login</Link>
-                        <Link className="btn" type="submit">Sign Up</Link>
-                    </form>
-                </div>
-            </div>
-        </nav>
-        </header>
-    )
-}
+const Header = ({ active }) => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            JobHuntly
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  to="/findjob"
+                  className={"nav-link ".concat(
+                    active === "find" ? "active" : ""
+                  )}
+                  aria-current="page"
+                  href="#"
+                >
+                  FindJobs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/browsecompanies"
+                  className={"nav-link ".concat(
+                    active === "browse" ? "active" : ""
+                  )}
+                >
+                  Browse Companies
+                </Link>
+              </li>
+            </ul>
+            {sessionStorage.getItem("userActive") === "true" ?
+            (
+                <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/img(31).jpg" className="rounded-circle" height="22"
+              alt="" loading="lazy" />
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+            //     <li className="nav-item me-3 me-lg-0 dropdown">
+            //     <Link className="nav-link dropdown-toggle" id="navbarDropdown1" role="button" data-mdb-toggle="dropdown"
+            //       aria-expanded="false">
+            //       <img src="https://mdbootstrap.com/img/Photos/Avatars/img(31).jpg" className="rounded-circle" height="22"
+            //         alt="" loading="lazy" />
+            //     </Link>
 
-export default Header
+            //   </li>
+            )
+            :
+            (
+              <form className="d-flex" role="search">
+                <Link to="/Login" className="btn" type="submit">
+                  Login
+                </Link>
+                <Link className="btn" type="submit">
+                  Sign Up
+                </Link>
+              </form>
+              )
+            }
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
