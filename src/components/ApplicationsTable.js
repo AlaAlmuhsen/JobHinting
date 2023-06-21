1dd32fa3b094f63231aa28c0793b85750ceba213
tasneem shehadeh <shhadahatasneem@gmail.com>
2023-06-21 13:10:04 +0300

    all applecatiopn page

--- src/components/ApplicationsTable.js
+++ src/components/ApplicationsTable.js
@@ -1,50 +1,126 @@
-import React from 'react'
+import React from "react";
 
-const ApplicationsTable = ({applications , companyData}) => {
+const ApplicationsTable = ({ applications, companyData }) => {
   return (
-    <table>
-        <thead>
-            <tr>
-                <th>first</th>
-                <th>last</th>
-                <th>major</th>
-                <th>birthday</th>
-                <th>status</th>
-                <th>phonenumber</th>
-                <th>timeOfApply</th>
-            </tr>
-        </thead>
-        <thead>
-            {applications
-            .filter((application) => {
-                return application.companyid == companyData.id
-            })
-            .map((application) => (
-                <tr key={application.id}>
-                    <td>{application.firstname}</td>
-                    <td>{application.lastname}</td>
-                    <td>{application.major}</td>
-                    <td>{application.birthday}</td>
-                    <td>{application.status}</td>
-                    <td>{application.phonenumber}</td>
-                    <td>{application.timeOfApply }</td>
-                </tr>
-            ))}
-        </thead>
-    </table>
-  )
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Total Applicants:19</a>
+          <form className="d-flex input-group w-auto">
+            <input
+              type="search"
+              className="form-control rounded"
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="search-addon"
+            />
+            <span className="input-group-text border-0" id="search-addon">
+              <i className="fas fa-search"></i>
+            </span>
+            <button type="button" className="btn btn-primary me-3">
+              Filter
+            </button>
+          </form>
+        </div>
+      </nav>
+      {applications
+        .filter((application) => {
+          return application.companyid == companyData.id;
+        })
+        .map((application) => (
+          <table
+            className="table align-middle bg-light text-dark"
+            key={application.id}
+          >
+            <thead className="bg-light">
+              <tr>
+                <th>Name</th>
+                <th>Major</th>
+                <th>Hiring Status</th>
+                <th>Application Date</th>
+                <th>Position</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="d-flex align-items-center">
+                    <img
+                      src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                      alt=""
+                      className="rounded-circle img-user-table"
+                    />
+                    <div className="ms-3">
+                      <p className="fw-bold mb-1">
+                        {application.firstname} {application.lastname}
+                      </p>
+                      <p className="text-muted mb-0">{application.email}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <p className="fw-normal mb-1">{application.major}</p>
+                </td>
+                <td>
+                  <span className="badge bg-info text-dark rounded-pill d-inline">
+                    {application.status}
+                  </span>
+                </td>
+                <td>
+                  <p className="text-muted mb-0">{application.timeOfApply}</p>
+                </td>
+
+                <td>
+                  <p className="fw-normal mb-1">{application.jobRole}</p>
+                  <p className="text-muted mb-0">{application.Position}</p>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-primary me-3">
+                    See Apllecation
+                  </button>
+                </td>
+              </tr>
+             
+            </tbody>
+          </table>
+        ))}
+    </>
+  );
+};
+
+export default ApplicationsTable;
+
+{
+  /* 
+// <table>
+    //     <thead>
+    //         <tr>
+    //             <th>first</th>
+    //             <th>last</th>
+    //             <th>major</th>
+    //             <th>birthday</th>
+    //             <th>status</th>
+    //             <th>phonenumber</th>
+    //             <th>timeOfApply</th>
+    //         </tr>
+    //     </thead>
+    //     <thead>
+    //         {applications
+    //         .filter((application) => {
+    //             return application.companyid == companyData.id
+    //         })
+    //         .map((application) => (
+    //             <tr key={application.id}>
+    //                 <td>{application.firstname}</td>
+    //                 <td>{application.lastname}</td>
+    //                 <td>{application.major}</td>
+    //                 <td>{application.birthday}</td>
+    //                 <td>{application.status}</td>
+    //                 <td>{application.phonenumber}</td>
+    //                 <td>{application.timeOfApply }</td>
+    //             </tr>
+    //         ))}
+    //     </thead>
+    // </table> */
 }
-
-export default ApplicationsTable
-
-
-// "companyid": "1",
-// "jobid" : "1",
-// "firstname" : "fname",
-// "lastname" : "lname",
-// "major" : "major",
-// "birthday" : "15/10/1998",
-// "status" : "pending",
-// "phonenumber" : "0777777777",
-// "timeOfApply" : "19/6/2023",
-// "id":"1"
