fc94b841d831ba65725db07f05a80160cef83b5a
Majd Jadallah <majd.nasser.jadallah@gmail.com>
2023-06-23 16:37:03 +0300

    pull

--- src/pages/Applied.js
+++ src/pages/Applied.js
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import {useNavigate} from "react-router-dom";
 function Applied() {
     const [firstname,setFirstname]=useState('');
     const [lastname,setLastname]=useState('');
-    const [email,setEmail]=useState('');
-    const [major,setMajor]=useState('');
     const [birthday,setBirthday]=useState('');
-    const [JobLevel,setJobLevel]=useState('');
     const [phoneNumber,setPhoneNumber]=useState('');
-    const [jobRole,setJobRole]=useState('');
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const application = {firstname, lastname, email,major,birthday,JobLevel,phoneNumber,jobRole };
+        const application = {companyid:"3",jobid:"2",firstname, lastname, email:'email',major:"major",birthday,status:"status",phoneNumber,timeOfApply:"20/12/1999",jobRole:'jobRole',JobLevel:'JobLevel' };
         fetch('http://localhost:5000/application/', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(application)
         }).then(() => {
           // history.go(-1);
-          navigate('/');
+          navigate('/findJob');
         })
       }
   return (
@@ -47,53 +43,16 @@ function Applied() {
             />
             <br></br>
             <br></br>
-
-            <label>email</label>
-            <input
-            type="text" 
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-            <br></br>
-            <br></br>
-
             <label>birthday</label>
             <input
-            type="text" 
-            required
-            value={major}
-            onChange={(e) => setMajor(e.target.value)}
-            />
-            <br></br>
-            <br></br>
-
-            <label>major</label>
-            <input
-            type="text"
+            type="date" 
             required
             value={birthday}
             onChange={(e) => setBirthday(e.target.value)}
             />
             <br></br>
             <br></br>
-
-            <label>status</label>
-            <input
-            type="text"
-            required
-            value={JobLevel}
-            onChange={(e) => setJobLevel(e.target.value)}
-            />
-            <br></br>
-            <br></br>
-
             <label>phone number</label>
-            <input/>
-            <br></br>
-            <br></br>
-
-            <label>job Role</label>
             <input
             type="text"
             required
@@ -102,14 +61,6 @@ function Applied() {
             />
             <br></br>
             <br></br>
-
-            <label>job level</label>
-            <input
-            type="text"
-            required
-            value={jobRole}
-            onChange={(e) => setJobRole(e.target.value)}
-            />
             <button>submit</button>
         </form>
       <Footer/>
