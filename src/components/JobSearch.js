56b4657bd3a3fbd2ac59bfd4189d66c19b075b8f
dania-atieh <dania.a.atieh@gmail.com>
2023-06-26 00:11:31 +0300

    added a functionality: search by the first letter that the user enters

--- src/components/JobSearch.js
+++ src/components/JobSearch.js
@@ -3,8 +3,11 @@ import { Row, Col, Card } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 
 function JobSearch() {
-  let concatenatedArray = []
-  let slicedArr = []
+  let concatenatedArray = [];
+  let categoryResult = [];
+  let joblevelResult = [];
+  let slicedArr = [];
+  let typeOfEmploymentResult = [];
   const [jobs, setJobs] = useState([]);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
@@ -57,10 +60,27 @@ function JobSearch() {
     setFilteredList(fetchResult);
   }, [jobs]);
 
+  // function inputFunction(e) {
+  //   setInputValue(e.target.value);
+  //   let searchResult = jobs.filter((job) =>
+  //     job.title.toLowerCase().includes(inputValue.toLowerCase())
+  //   );
+  //   setFilteredList(searchResult);
+  // }
+  
+  // function compareStrings(str1, str2) {
+  //   for(let i = 0 ; i < str2.length; ++i){
+  //      str1.charAt(i) === str2.charAt(i)
+  //     }
+   
+  //   return str1;
+  //   console.log(str1)
+  // }
+
   function inputFunction(e) {
     setInputValue(e.target.value);
     let searchResult = jobs.filter((job) =>
-      job.title.toLowerCase().includes(inputValue.toLowerCase())
+    job.title.toLowerCase().startsWith(inputValue.toLowerCase())
     );
     setFilteredList(searchResult);
   }
@@ -68,284 +88,243 @@ function JobSearch() {
   //added Part-time for the 9 ids
 
   useEffect(() => {
-     if(category === undefined)
-    setFilteredList(jobs);
+    if (category === undefined) setFilteredList(jobs);
   }, [jobs]);
 
   //function get the value of the checkbox
   const handleCheckboxValue = (e) => {
-    let value = "";
+    // e.target.checked && (setccategoryValue({
+    //   ...categoryValue,
+    //   [`${e.target.value}`]: true,
+    // })
+    // );
 
-    value = e.target.checked === true ? e.target.value : e.target.id;
+    //update if properties false or true
+    e.target.checked
+      ? (typeOfEmploymentValue[`${e.target.value}`] = true)
+      : (typeOfEmploymentValue[`${e.target.value}`] = false);
 
-    //when checked
-    value === "Full-time" && (typeOfEmploymentValue["Full-time"] = true);
+    e.target.checked
+      ? (categoryValue[`${e.target.value}`] = true)
+      : (categoryValue[`${e.target.value}`] = false);
 
-    value === "Part-time" && (typeOfEmploymentValue["Part-time"] = true);
-
-    value === "Technology" && (categoryValue["Technology"] = true);
-
-    value === "Engineering" && (categoryValue["Engineering"] = true);
-
-    value === "Business" && (categoryValue["Business"] = true);
-
-    value === "Meadical" && (categoryValue["Meadical"] = true);
-
-    value === "Sales" && (categoryValue["Sales"] = true);
-
-    value === "Marketing" && (categoryValue["Marketing"] = true);
-
-    value === "Accounting" && (categoryValue["Accounting"] = true);
-
-    value === "Teaching" && (categoryValue["Teaching"] = true);
-
-    value === "Senior" && (joblevelValue["Senior"] = true);
-
-    value === "Junior" && (joblevelValue["Junior"] = true);
-
-    //when not checked working on ID
-    value === "Fulltime" && (typeOfEmploymentValue["Full-time"] = false);
-
-    value === "Parttime" && (typeOfEmploymentValue["Part-time"] = false);
-
-    value === "technology" && (categoryValue["Technology"] = false);
-
-    value === "engineering" && (categoryValue["Engineering"] = false);
-
-    value === "business" && (categoryValue["Business"] = false);
-
-    value === "meadical" && (categoryValue["Meadical"] = false);
-
-    value === "sales" && (categoryValue["Sales"] = false);
-
-    value === "marketing" && (categoryValue["Marketing"] = false);
-
-    value === "accounting" && (categoryValue["Accounting"] = false);
-
-    value === "teaching" && (categoryValue["Teaching"] = false);
-
-    value === "senior" && (joblevelValue["Senior"] = false);
-
-    value === "junior" && (joblevelValue["Junior"] = false);
+    e.target.checked
+      ? (joblevelValue[`${e.target.value}`] = true)
+      : (joblevelValue[`${e.target.value}`] = false);
 
     //filters section
-
     //typeOfEmployment filter section
     let truetypeOfEmployment = Object.keys(typeOfEmploymentValue).filter(
       (prop) => typeOfEmploymentValue[prop] === true
     );
 
-    let typeOfEmploymentResult = truetypeOfEmployment.map((prop) =>
-      jobs.filter((job) => job.typeOfEmployment === prop)
-    );
-
-    if(typeOfEmploymentResult != null){
-    // setFilteredList(typeOfEmploymentResult[0]);
-    concatenatedArray = [].concat(...typeOfEmploymentResult);
-    filteredList = concatenatedArray
-  }
+    if (truetypeOfEmployment) {
+      typeOfEmploymentResult = truetypeOfEmployment.map((prop) =>
+        jobs.filter((job) => job.typeOfEmployment === prop)
+      );
+      concatenatedArray = [].concat(...typeOfEmploymentResult);
+      // filteredList = concatenatedArray;
+    }
 
     // Category filter section
     let trueCategories = Object.keys(categoryValue).filter(
       (prop) => categoryValue[prop] === true
     );
 
-    let categoryResult = trueCategories.map((prop) =>
-      jobs.filter((job) => job.category === prop)
-    );
+    if (trueCategories) {
+      categoryResult = trueCategories.map((prop) =>
+        jobs.filter((job) => job.category === prop)
+      );
 
-    if(categoryResult != null){
       concatenatedArray = [].concat(...categoryResult);
-        filteredList = concatenatedArray
-        // filteredList = filteredList.concat(...concatenatedArray)
+      // filteredList = concatenatedArray;
     }
-   
+
     // Job level filter section
     let truejoblevel = Object.keys(joblevelValue).filter(
       (prop) => joblevelValue[prop] === true
     );
 
-    let joblevelResult = truejoblevel.map((prop) =>
-      jobs.filter((job) => job.jobLevel === prop)
-    );
-
-    if(joblevelResult != null){
+    if (truejoblevel) {
+      joblevelResult = truejoblevel.map((prop) =>
+        jobs.filter((job) => job.jobLevel === prop)
+      );
       concatenatedArray = [].concat(...joblevelResult);
     }
-    
-setFilteredList(concatenatedArray)
-  };
-  
-    if(filteredList != undefined){
-          slicedArr = filteredList.slice(0, 12);
-    }
 
+    setFilteredList(concatenatedArray);
+    console.log(concatenatedArray)
+  };
+
+  if (filteredList != undefined) {
+    slicedArr = filteredList.slice(0, 12);
+  }
 
   return (
     <>
-    <div className="col-3 p-0">
-      <div style={{ color: "white" }}>
-        <h6>Type of Employment</h6>
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="Fulltime">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Full-time"
-              id="Fulltime"
-              onChange={handleCheckboxValue}
-            />
-            Full-time
-          </label>
-        </div>
+      <div className="col-3 p-0">
+        <div style={{ color: "white" }}>
+          <h6>Type of Employment</h6>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="Fulltime">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Full-time"
+                id="Fulltime"
+                onChange={handleCheckboxValue}
+              />
+              Full-time
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="Parttime">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Part-time"
-              id="Parttime"
-              onChange={handleCheckboxValue}
-            />
-            Part-time
-          </label>
-        </div>
-        <br />
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="Parttime">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Part-time"
+                id="Parttime"
+                onChange={handleCheckboxValue}
+              />
+              Part-time
+            </label>
+          </div>
+          <br />
 
-        <h6>Categories</h6>
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="technology">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Technology"
-              id="technology"
-              onChange={handleCheckboxValue}
-            />
-            Technology
-          </label>
-        </div>
+          <h6>Categories</h6>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="technology">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Technology"
+                id="technology"
+                onChange={handleCheckboxValue}
+              />
+              Technology
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="engineering">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Engineering"
-              id="engineering"
-              onChange={handleCheckboxValue}
-            />
-            Engineering
-          </label>
-        </div>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="engineering">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Engineering"
+                id="engineering"
+                onChange={handleCheckboxValue}
+              />
+              Engineering
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="business">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Business"
-              id="business"
-              onChange={handleCheckboxValue}
-            />
-            Business
-          </label>
-        </div>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="business">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Business"
+                id="business"
+                onChange={handleCheckboxValue}
+              />
+              Business
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="meadical">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Meadical"
-              id="meadical"
-              onChange={handleCheckboxValue}
-            />
-            Meadical
-          </label>
-        </div>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="meadical">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Meadical"
+                id="meadical"
+                onChange={handleCheckboxValue}
+              />
+              Meadical
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="sales">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Sales"
-              id="sales"
-              onChange={handleCheckboxValue}
-            />
-            Sales
-          </label>
-        </div>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="sales">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Sales"
+                id="sales"
+                onChange={handleCheckboxValue}
+              />
+              Sales
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="marketing">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Marketing"
-              id="marketing"
-              onChange={handleCheckboxValue}
-            />
-            Marketing
-          </label>
-        </div>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="marketing">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Marketing"
+                id="marketing"
+                onChange={handleCheckboxValue}
+              />
+              Marketing
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="accounting">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Accounting"
-              id="accounting"
-              onChange={handleCheckboxValue}
-            />
-            Accounting
-          </label>
-        </div>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="accounting">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Accounting"
+                id="accounting"
+                onChange={handleCheckboxValue}
+              />
+              Accounting
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="teaching">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Teaching"
-              id="teaching"
-              onChange={handleCheckboxValue}
-            />
-            Teaching
-          </label>
-        </div>
-        <br />
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="teaching">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Teaching"
+                id="teaching"
+                onChange={handleCheckboxValue}
+              />
+              Teaching
+            </label>
+          </div>
+          <br />
 
-        <h6>Job Level</h6>
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="senior">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Senior"
-              id="senior"
-              onChange={handleCheckboxValue}
-            />
-            Senior
-          </label>
-        </div>
+          <h6>Job Level</h6>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="senior">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Senior"
+                id="senior"
+                onChange={handleCheckboxValue}
+              />
+              Senior
+            </label>
+          </div>
 
-        <div className="form-check">
-          <label className="form-check-label" htmlFor="Junior">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="Junior"
-              id="Junior"
-              onChange={handleCheckboxValue}
-            />
-            Junior
-          </label>
+          <div className="form-check">
+            <label className="form-check-label" htmlFor="Junior">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Junior"
+                id="Junior"
+                onChange={handleCheckboxValue}
+              />
+              Junior
+            </label>
+          </div>
         </div>
-      </div>
       </div>
       {/* end of filter section */}
       <div className="col-9 p-0">
@@ -354,6 +333,7 @@ setFilteredList(concatenatedArray)
           <input
             placeholder="Enter the job search"
             value={inputValue}
+            onBlur={inputFunction}
             onChange={inputFunction}
             id="inputField"
           />
@@ -361,8 +341,7 @@ setFilteredList(concatenatedArray)
         {isPending && <p>Loading...</p>}
         {error && <p>{error}</p>}
         <Row>
-          {
-          slicedArr.map((job) => (
+          {slicedArr.map((job) => (
             <Col key={job.id} md={6} className="h-100">
               <Card id="card">
                 <Card.Body>
@@ -380,7 +359,7 @@ setFilteredList(concatenatedArray)
             </Col>
           ))}
         </Row>
-        </div>
+      </div>
     </>
   );
 }
